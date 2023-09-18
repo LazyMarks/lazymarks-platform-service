1a501b20d1be5216258827f795f4019f513eb72a
Ankit Raj <rajankit420@gmail.com>
2023-09-18 16:56:04 +0530

    First commit

---
+++ src/main/java/com/lazymarks/platform/api/LazyMarksPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.lazymarks.platform.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyMarksPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyMarksPlatformApiApplication.class, args);
+	}
+
+}
